a1929492d74a2f21e56dc08c491a478a83cf79b2
Sangram <sangramkesharichoudhury6@gmail.com>
2023-01-07 14:42:34 +0530

    FBVproject

---
+++ fbvapp/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.contrib import admin
+from django.urls import path
+from .views import courseListView , courseDetailView
+
+urlpatterns = [
+    path('courses', courseListView),
+    path('courses<int:pk>', courseDetailView),
+]
